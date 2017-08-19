498b2d9510c8e45d1f78bd69c9092cd2fa42420e
DESKTOP-780STAN\santi <sngtdeandrea@gmail.com>
2017-08-19 17:51:53 -0300

    ...

--- src/farmacia/Farmacia.java
+++ src/farmacia/Farmacia.java
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 package farmacia;
 
 public class Farmacia {
 
 }
-=======
-package farmacia;
-
-public class Farmacia {
-
-}
->>>>>>> branch 'master' of https://github.com/santiagodea/farmacia.git
