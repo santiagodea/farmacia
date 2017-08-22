855b7eeaa75536a93e4399ee9846dfe12de04161
DESKTOP-780STAN\santi <sngtdeandrea@gmail.com>
2017-08-22 17:42:25 -0300

    ...

---
+++ src/farmacia/ObraSocial.java
@@ -0,0 +1,10 @@
+package farmacia;
+
+public class ObraSocial {
+	private String nombre;
+
+	//getters & setters
+	public String getNombre() {return nombre;}
+	public void setNombre(String nombre) {this.nombre = nombre;}
+	
+}
